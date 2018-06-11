082b512cad9619205a90e07a4d1c5e71ec21a692
Jack DeMeyers <319723@wegmans.com>
2018-06-11 12:05:26 -0400

    Refactor Block and Game2048 classes to improve algorithm readability.

--- src/2048/game2048.ts
+++ src/2048/game2048.ts
@@ -1,251 +1,133 @@
 import { Block } from "./block";
 import { Position } from "./position";
+import { Direction } from "./direction";
 
 export class Game2048 {
   grid: Array<Array<Block>>;
-  render: Boolean;
+  needNew: Boolean;
 
-  constructor(render?: Boolean) {
-    this.grid = this.createGrid();
-    this.initGrid();
-    this.showGame();
-    this.render = render != null ? render : true;
+  constructor() {
+    this._initGrid();
+    this.needNew = false;
+    this.display();
   }
 
-  showGame(): void {
-    if (this.render) {
-      let grid = '';
-      for (let j = 0; j < 4; j++) {
-        for (let i = 0; i < 4; i++) {
-          grid += this.grid[i][j].value || '_';
-          grid += '|';
-        }
-        grid += '\n';
+  display(): void {
+    let grid: string = "";
+    for (let j: number = 0; j < 4; j++) {
+      for (let i: number = 0; i < 4; i++) {
+        grid += this.grid[i][j].value || "_";
+        grid += "|";
       }
-      console.log(grid);
+      grid += "\n";
     }
+    console.log(grid);
   }
 
-  swipeLeft(): void {
-    let needNew: Boolean = false;
-    // combine 
-    for (let j = 0; j < 4; j++) {
-      for (let i = 0; i < 4; i++) {
-        if (!this.grid[i][j].open) {
-          for (let k = i + 1; k < 4; k++) {
-            if (!this.grid[k][j].open) {
-              if (this.valuesMatch(this.grid[i][j], this.grid[k][j])) {
-                needNew = true;
-                this.combine(this.grid[i][j], this.grid[k][j]);
-              }
-              break;
-            }
-          }
-        }
-      }
+  swipe(dir: Direction): void {
+    switch (dir) {
+      case Direction.Left:
+        this._swipeLeft();
+        break;
+      case Direction.Right:
+        this._swipeRight();
+        break;
+      case Direction.Up:
+        this._swipeUp();
+        break;
+      case Direction.Down:
+        this._swipeDown();
+        break;
     }
 
-    // move
-    for (let j = 0; j < 4; j++) {
-      for (let i = 0; i < 4; i++) {
-        if (this.grid[i][j].open) {
-          for (let k = i + 1; k < 4; k++) {
-            if (!this.grid[k][j].open && i != k) {
-              needNew = true;
-              this.grid[i][j].value = this.grid[k][j].value;
-              this.grid[k][j].open = true;
-              this.grid[i][j].open = false;
-              break;
-            }
-          }
-        }
-      }
+    if (this.needNew) {
+      this._addNewBlock();
+      this.display();
     }
+    this._resetCombineFlags();
+  }
 
-    // add
-    if (needNew == true) {
-      this.addNew();
-      this.showGame();
+  private _swipeLeft(): void {
+    for (let j: number = 0; j < 4; j++) {
+      for (let i: number = 0; i < 4; i++) {
+        this._attemptSwipe(i, j, Direction.Left);
+      }
     }
   }
 
-  swipeRight(): void {
-    let needNew: Boolean = false;
-    // combine 
-    for (let j = 0; j < 4; j++) {
-      for (let i = 3; i >= 0; i--) {
-        if (!this.grid[i][j].open) {
-          for (let k = i - 1; k >= 0; k--) {
-            if (!this.grid[k][j].open) {
-              if (this.valuesMatch(this.grid[i][j], this.grid[k][j])) {
-                needNew = true;
-                this.combine(this.grid[i][j], this.grid[k][j]);
-              }
-              break;
-            }
-          }
-        }
+  private _swipeRight(): void {
+    for (let j: number = 0; j < 4; j++) {
+      for (let i: number = 3; i >= 0; i--) {
+        this._attemptSwipe(i, j, Direction.Right);
       }
-    }
-
-    // move
-    for (let j = 0; j < 4; j++) {
-      for (let i = 3; i >= 0; i--) {
-        if (this.grid[i][j].open) {
-          for (let k = i - 1; k >= 0; k--) {
-            if (!this.grid[k][j].open && i != k) {
-              this.grid[i][j].value = this.grid[k][j].value;
-              this.grid[k][j].open = true;
-              this.grid[i][j].open = false;
-              break;
-            }
-          }
-        }
-      }
-    }
-
-    // add
-    if (needNew == true) {
-      this.addNew();
-      this.showGame();
     }
   }
 
-  swipeUp(): void {
-    let needNew: Boolean = false;
-    // combine 
-    for (let i = 0; i < 4; i++) {
-      for (let j = 0; j < 4; j++) {
-        if (!this.grid[i][j].open) {
-          for (let k = j + 1; k < 4; k++) {
-            if (!this.grid[i][k].open) {
-              if (this.valuesMatch(this.grid[i][j], this.grid[i][k])) {
-                needNew = true;
-                this.combine(this.grid[i][j], this.grid[i][k]);
-              }
-              break;
-            }
-          }
-        }
+  private _swipeUp(): void {
+    for (let i: number = 0; i < 4; i++) {
+      for (let j: number = 0; j < 4; j++) {
+        this._attemptSwipe(i, j, Direction.Up);
       }
-    }
-
-    // move
-    for (let i = 0; i < 4; i++) {
-      for (let j = 0; j < 4; j++) {
-        if (this.grid[i][j].open) {
-          for (let k = j + 1; k < 4; k++) {
-            if (!this.grid[i][k].open && j != k) {
-              needNew = true;
-              this.grid[i][j].value = this.grid[i][k].value;
-              this.grid[i][k].open = true;
-              this.grid[i][j].open = false;
-              break;
-            }
-          }
-        }
-      }
-    }
-
-    // add
-    if (needNew == true) {
-      this.addNew();
-      this.showGame();
     }
   }
 
-  swipeDown(): void {
-    let needNew: Boolean = false;
-    // combine 
-    for (let i = 0; i < 4; i++) {
-      for (let j = 3; j >= 0; j--) {
-        if (!this.grid[i][j].open) {
-          for (let k = j - 1; k >= 0; k--) {
-            if (!this.grid[i][k].open) {
-              if (this.valuesMatch(this.grid[i][j], this.grid[i][k])) {
-                needNew = true;
-                this.combine(this.grid[i][j], this.grid[i][k]);
-              }
-              break;
-            }
-          }
-        }
+  private _swipeDown(): void {
+    for (let i: number = 0; i < 4; i++) {
+      for (let j: number = 3; j >= 0; j--) {
+        this._attemptSwipe(i, j, Direction.Down);
       }
-    }
-
-    // move
-    for (let i = 0; i < 4; i++) {
-      for (let j = 3; j >= 0; j--) {
-        if (this.grid[i][j].open) {
-          for (let k = j - 1; k >= 0; k--) {
-            if (!this.grid[i][k].open && j != k) {
-              needNew = true;
-              this.grid[i][j].value = this.grid[i][k].value;
-              this.grid[i][k].open = true;
-              this.grid[i][j].open = false;
-              break;
-            }
-          }
-        }
-      }
-    }
-
-    // add
-    if (needNew == true) {
-      this.addNew();
-      this.showGame();
     }
   }
 
-  private addNew(): void {
-    let p = new Position();
+  private _attemptSwipe(i: number, j: number, dir: Direction): void {
+    let curBlock: Block = this.grid[i][j];
+    if (!curBlock.open) {
+      while (curBlock.canMove(dir, this.grid)) {
+        let neighbor: Block = curBlock.getNeighbor(dir, this.grid);
+        curBlock.moveTo(neighbor);
+        curBlock = neighbor;
+        this.needNew = true;
+      }
+    }
+  }
+
+  private _resetCombineFlags(): void {
+    for (let i: number = 0; i < 4; i++) {
+      for (let j: number = 0; j < 4; j++) {
+        this.grid[i][j].resetCombineFlag();
+      }
+    }
+  }
+
+  private _initGrid(): void {
+    this._createGrid();
+    this._fillGrid();
+    this._addNewBlock();
+    this._addNewBlock();
+  }
+
+  private _createGrid(): void {
+    this.grid = new Array(4);
+    for (let i: number = 0; i < 4; i++) {
+      this.grid[i] = new Array(4);
+    }
+  }
+
+  private _fillGrid(): void {
+    for (let i: number = 0; i < this.grid.length; i++) {
+      for (let j: number = 0; j < this.grid[i].length; j++) {
+        this.grid[i][j] = new Block(new Position(i, j));
+      }
+    }
+  }
+
+  private _addNewBlock(): void {
+    let p: Position = new Position();
     while (!this.grid[p.x][p.y].open) {
       p = new Position();
     }
-    this.grid[p.x][p.y].val = Math.random() >= 0.3 ? 2 : 4;
+    this.grid[p.x][p.y].value = Math.random() >= 0.25 ? 2 : 4;
     this.grid[p.x][p.y].open = false;
-  }
-
-  private valuesMatch(b1: Block, b2: Block): Boolean {
-    return b1.value == b2.value;
-  }
-
-  private combine(b1: Block, b2: Block) {
-    b1.value *= 2;
-    b2.open = true;
-  }
-
-  private createGrid(): Array<Array<Block>> {
-    let grid = new Array(4);
-    for (let i = 0; i < 4; i++) {
-      grid[i] = new Array(4);
-    }
-    return grid;
-  }
-
-  private initGrid(): void {
-    this.fillGrid();
-    this.setInitBlocks();
-  }
-
-  private fillGrid(): void {
-    for (let i = 0; i < this.grid.length; i++) {
-      for (let j = 0; j < this.grid[i].length; j++) {
-        this.grid[i][j] = new Block();
-      }
-    }
-  }
-
-  private setInitBlocks(): void {
-    let p1 = new Position();
-    let p2;
-    while (true) {
-      p2 = new Position();
-      if (p1 != p2) break;
-    }
-    this.grid[p1.x][p1.y].open = false;
-    this.grid[p1.x][p1.y].value = 2;
-    this.grid[p2.x][p2.y].open = false;
-    this.grid[p2.x][p2.y].value = 2;
+    this.needNew = false;
   }
 }
